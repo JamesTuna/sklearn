40d3a04e6ac4e9a18e31039d30463a708291da7b
JamesTuna <dzhuocheng@gmail.com>
2018-03-15 16:18:06 +0800

    fix wrong indexing

--- optional.py
+++ optional.py
@@ -13,32 +13,28 @@ y_test = [[8.3], [12.5], [15.4], [19.6]]
 
 # iterate all degrees,
 # and get the maximum prediction degreee
-pr_score=[]
-ridge_score=[]
-for i  in range(0,20):
+pr_d = 0
+rg_d = 0
+max_pr = -999
+max_rg = -999
+for i  in range(1,21):
     poly = PolynomialFeatures(degree=i)
     X_train_poly = poly.fit_transform(X_train)
     X_test_poly = poly.transform(X_test)
-
     # build a linear regression model
     # and record the scores
     pr_model = LinearRegression()
     pr_model.fit(X_train_poly,y_train)
-    pr_score.append(pr_model.score(X_test_poly,y_test))
-
+    if(pr_model.score(X_test_poly,y_test)>max_pr):
+        max_pr = pr_model.score(X_test_poly,y_test)
+        pr_d = i
     # build a linear regression model
     # and record the scores
     ridge_model = Ridge(alpha=1, normalize=False)
     ridge_model.fit(X_train_poly, y_train)
-    ridge_score.append(ridge_model.score(X_test_poly,y_test))
-'''
-print("pr_score")
-print(pr_score)
-print("")
-print("ridge_score")
-print(ridge_score)
-print("")
-'''
+    if(ridge_model.score(X_test_poly,y_test)>max_rg):
+        max_rg = ridge_model.score(X_test_poly,y_test)
+        rg_d = i
 # print the maximum degreee of score
-print("max pr score: ", max(pr_score), "degree: ",pr_score.index(max(pr_score))+1)
-print("max ridge score: ", max(ridge_score), "degree: ",pr_score.index(max(pr_score))+1)
+print("max polynomial regression score: ", max_pr, "degree: ",pr_d)
+print("max ridge score: ", max_rg, "degree: ",rg_d)
